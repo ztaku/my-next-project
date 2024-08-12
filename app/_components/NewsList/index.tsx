7ebc2e2a7e524c17ed70ab1f114e46a189d5754c
ztaku <watanabe@zblog.sakura.ne.jp>
2024-08-12 15:52:04 +0900

    5章完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -17,15 +17,24 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          {/* TODO:別の章で、リンクと正式な画像に差し替えます */}
           <Link href={`/news/${article.id}`} className={styles.link}>
-            <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-            />
+            { article.thumbnail ? (
+              <Image
+                className={styles.image}
+                src={article.thumbnail.url}
+                alt=""
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ):(
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            ) }
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,14 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
-const data: {
-  contents: News[] } = {
-    contents: [
-      {
-        id: "1",
-        title: "渋谷に移転",
-        category: {
-          name: "更新情報",
-        },
-        publishedAt: "2023/05/19",
-        createdAt: "2023/05/19",
-      },
-      {
-        id: "2",
-        title: "当社CEO",
-        category: {
-          name: "更新情報",
-        },
-        publishedAt: "2023/05/19",
-        createdAt: "2023/05/19",
-      },
-      {
-        id: "3",
-        title: "TEST",
-        category: {
-          name: "更新情報",
-        },
-        publishedAt: "2023/04/19",
-        createdAt: "2023/04/19",
-      },
-    ],
-  }
-
-
-export default function Home() {
-  const sliceData = data.contents.slice(0,2);
+export default async function Home() {
+  const data = await getNewsList({
+    limit:TOP_NEWS_LIMIT,
+  })
   return (
     <>
     <section className={styles.top}>
@@ -51,7 +20,7 @@ export default function Home() {
     </section>
     <section className={styles.news}>
       <h2 className={styles.newsTitle}>News</h2>
-      <NewsList news={sliceData} />
+      <NewsList news={data.contents} />
       <div className={styles.newsLink}>
         <ButtonLink href="/news">もっと見る</ButtonLink>
       </div>
